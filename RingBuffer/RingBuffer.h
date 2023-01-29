6188cf1065532c60c53be8697ed874b3e77dcdbc
unknown <475641843@qq.com>
2023-01-29 09:23:00 +0800

    调整缩进

--- RingBuffer/RingBuffer.h
+++ RingBuffer/RingBuffer.h
@@ -2,7 +2,8 @@
 #define __RINGBUFFER_H
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
 /* Header includes -----------------------------------------------------------*/
@@ -11,17 +12,17 @@ extern "C" {
 #include <stdlib.h>
 
 /* Macro definitions ---------------------------------------------------------*/
-#define RING_BUFFER_MALLOC(size)  malloc(size)
-#define RING_BUFFER_FREE(block)   free(block)
+#define RING_BUFFER_MALLOC(size) malloc(size)
+#define RING_BUFFER_FREE(block)  free(block)
 
 /* Type definitions ----------------------------------------------------------*/
 typedef struct
 {
-  uint8_t *buffer;
-  uint32_t size;
-  uint32_t in;
-  uint32_t out;
-}RingBuffer;
+	uint8_t *buffer;
+	uint32_t size;
+	uint32_t in;
+	uint32_t out;
+} RingBuffer;
 
 /* Variable declarations -----------------------------------------------------*/
 /* Variable definitions ------------------------------------------------------*/
@@ -35,65 +36,65 @@ uint32_t RingBuffer_Out(RingBuffer *fifo, void *out, uint32_t len);
 /* Function definitions ------------------------------------------------------*/
 
 /**
-  * @brief  Removes the entire FIFO contents.
-  * @param  [in] fifo: The fifo to be emptied.
-  * @return None.
-  */
+ * @brief  Removes the entire FIFO contents.
+ * @param  [in] fifo: The fifo to be emptied.
+ * @return None.
+ */
 static inline void RingBuffer_Reset(RingBuffer *fifo)
 {
-  fifo->in = fifo->out = 0;
+	fifo->in = fifo->out = 0;
 }
 
 /**
-  * @brief  Returns the size of the FIFO in bytes.
-  * @param  [in] fifo: The fifo to be used.
-  * @return The size of the FIFO.
-  */
+ * @brief  Returns the size of the FIFO in bytes.
+ * @param  [in] fifo: The fifo to be used.
+ * @return The size of the FIFO.
+ */
 static inline uint32_t RingBuffer_Size(RingBuffer *fifo)
 {
-  return fifo->size;
+	return fifo->size;
 }
 
 /**
-  * @brief  Returns the number of used bytes in the FIFO.
-  * @param  [in] fifo: The fifo to be used.
-  * @return The number of used bytes.
-  */
+ * @brief  Returns the number of used bytes in the FIFO.
+ * @param  [in] fifo: The fifo to be used.
+ * @return The number of used bytes.
+ */
 static inline uint32_t RingBuffer_Len(RingBuffer *fifo)
 {
-  return fifo->in - fifo->out;
+	return fifo->in - fifo->out;
 }
 
 /**
-  * @brief  Returns the number of bytes available in the FIFO.
-  * @param  [in] fifo: The fifo to be used.
-  * @return The number of bytes available.
-  */
+ * @brief  Returns the number of bytes available in the FIFO.
+ * @param  [in] fifo: The fifo to be used.
+ * @return The number of bytes available.
+ */
 static inline uint32_t RingBuffer_Avail(RingBuffer *fifo)
 {
-  return RingBuffer_Size(fifo) - RingBuffer_Len(fifo);
+	return RingBuffer_Size(fifo) - RingBuffer_Len(fifo);
 }
 
 /**
-  * @brief  Is the FIFO empty?
-  * @param  [in] fifo: The fifo to be used.
-  * @retval true:      Yes.
-  * @retval false:     No.
-  */
+ * @brief  Is the FIFO empty?
+ * @param  [in] fifo: The fifo to be used.
+ * @retval true:      Yes.
+ * @retval false:     No.
+ */
 static inline bool RingBuffer_IsEmpty(RingBuffer *fifo)
 {
-  return RingBuffer_Len(fifo) == 0;
+	return RingBuffer_Len(fifo) == 0;
 }
 
 /**
-  * @brief  Is the FIFO full?
-  * @param  [in] fifo: The fifo to be used.
-  * @retval true:      Yes.
-  * @retval false:     No.
-  */
+ * @brief  Is the FIFO full?
+ * @param  [in] fifo: The fifo to be used.
+ * @retval true:      Yes.
+ * @retval false:     No.
+ */
 static inline bool RingBuffer_IsFull(RingBuffer *fifo)
 {
-  return RingBuffer_Avail(fifo) == 0;
+	return RingBuffer_Avail(fifo) == 0;
 }
 
 #ifdef __cplusplus
--- RingBuffer/main.c
+++ RingBuffer/main.c
@@ -11,317 +11,316 @@
 /* Function definitions ------------------------------------------------------*/
 
 /**
-  * @brief  Main program.
-  * @param  None.
-  * @return None.
-  */
+ * @brief  Main program.
+ * @param  None.
+ * @return None.
+ */
 int main(void)
 {
-  uint8_t data[256] = {0};
+	uint8_t data[256] = {0};
 
-  for(int i = 0; i < sizeof(data); i++)
-  {
-    data[i] = i;
-  }
+	for (int i = 0; i < sizeof(data); i++)
+	{
+		data[i] = i;
+	}
 
-  RingBuffer *fifo = RingBuffer_Malloc(sizeof(data));
+	RingBuffer *fifo = RingBuffer_Malloc(sizeof(data));
 
-  if(fifo != NULL)
-  {
-    printf("FIFO创建成功，FIFO大小：%d，使用大小：%d，剩余大小：%d\n",
-           RingBuffer_Size(fifo), RingBuffer_Len(fifo), RingBuffer_Avail(fifo));
+	if (fifo != NULL)
+	{
+		printf("FIFO创建成功，FIFO大小：%d，使用大小：%d，剩余大小：%d\n",
+			   RingBuffer_Size(fifo), RingBuffer_Len(fifo), RingBuffer_Avail(fifo));
 
-    if(RingBuffer_IsFull(fifo) == true)
-    {
-      printf("FIFO满了！！！\n");
-    }
-    else
-    {
-      printf("FIFO没满！！！\n");
-    }
+		if (RingBuffer_IsFull(fifo) == true)
+		{
+			printf("FIFO满了！！！\n");
+		}
+		else
+		{
+			printf("FIFO没满！！！\n");
+		}
 
-    if(RingBuffer_IsEmpty(fifo) == true)
-    {
-      printf("FIFO空了！！！\n");
-    }
-    else
-    {
-      printf("FIFO没空！！！\n");
-    }
+		if (RingBuffer_IsEmpty(fifo) == true)
+		{
+			printf("FIFO空了！！！\n");
+		}
+		else
+		{
+			printf("FIFO没空！！！\n");
+		}
 
-    printf("\n");
+		printf("\n");
 
-    for(;;)
-    {
-      {
-        if(RingBuffer_In(fifo, data, sizeof(data) / 2) > 0)
-        {
-          printf("FIFO写入成功，FIFO大小：%d，使用大小：%d，剩余大小：%d\n",
-                 RingBuffer_Size(fifo), RingBuffer_Len(fifo), RingBuffer_Avail(fifo));
-        }
-        else
-        {
-          printf("FIFO写入失败，FIFO大小：%d，使用大小：%d，剩余大小：%d\n",
-                 RingBuffer_Size(fifo), RingBuffer_Len(fifo), RingBuffer_Avail(fifo));
-        }
+		for (;;)
+		{
+			{
+				if (RingBuffer_In(fifo, data, sizeof(data) / 2) > 0)
+				{
+					printf("FIFO写入成功，FIFO大小：%d，使用大小：%d，剩余大小：%d\n",
+						   RingBuffer_Size(fifo), RingBuffer_Len(fifo), RingBuffer_Avail(fifo));
+				}
+				else
+				{
+					printf("FIFO写入失败，FIFO大小：%d，使用大小：%d，剩余大小：%d\n",
+						   RingBuffer_Size(fifo), RingBuffer_Len(fifo), RingBuffer_Avail(fifo));
+				}
 
-        if(RingBuffer_IsFull(fifo) == true)
-        {
-          printf("FIFO满了！！！\n");
-        }
-        else
-        {
-          printf("FIFO没满！！！\n");
-        }
+				if (RingBuffer_IsFull(fifo) == true)
+				{
+					printf("FIFO满了！！！\n");
+				}
+				else
+				{
+					printf("FIFO没满！！！\n");
+				}
 
-        if(RingBuffer_IsEmpty(fifo) == true)
-        {
-          printf("FIFO空了！！！\n");
-        }
-        else
-        {
-          printf("FIFO没空！！！\n");
-        }
-      }
+				if (RingBuffer_IsEmpty(fifo) == true)
+				{
+					printf("FIFO空了！！！\n");
+				}
+				else
+				{
+					printf("FIFO没空！！！\n");
+				}
+			}
 
-      printf("\n");
+			printf("\n");
 
-      {
-        uint8_t rdata[64] = {0};
-        uint8_t len       = RingBuffer_Out(fifo, rdata, sizeof(rdata));
+			{
+				uint8_t rdata[64] = {0};
+				uint8_t len = RingBuffer_Out(fifo, rdata, sizeof(rdata));
 
-        if(len > 0)
-        {
-          printf("从FIFO中读出的数据，长度：%d\n", len);
+				if (len > 0)
+				{
+					printf("从FIFO中读出的数据，长度：%d\n", len);
 
-          for(int i = 0; i < len; i++)
-          {
-            printf("%d ", rdata[i]);
-          }
+					for (int i = 0; i < len; i++)
+					{
+						printf("%d ", rdata[i]);
+					}
 
-          printf("\n");
+					printf("\n");
 
-          printf("FIFO读取成功，FIFO大小：%d，使用大小：%d，剩余大小：%d\n",
-                 RingBuffer_Size(fifo), RingBuffer_Len(fifo), RingBuffer_Avail(fifo));
-        }
-        else
-        {
-          printf("FIFO读取失败，FIFO大小：%d，使用大小：%d，剩余大小：%d\n",
-                 RingBuffer_Size(fifo), RingBuffer_Len(fifo), RingBuffer_Avail(fifo));
-        }
+					printf("FIFO读取成功，FIFO大小：%d，使用大小：%d，剩余大小：%d\n",
+						   RingBuffer_Size(fifo), RingBuffer_Len(fifo), RingBuffer_Avail(fifo));
+				}
+				else
+				{
+					printf("FIFO读取失败，FIFO大小：%d，使用大小：%d，剩余大小：%d\n",
+						   RingBuffer_Size(fifo), RingBuffer_Len(fifo), RingBuffer_Avail(fifo));
+				}
 
-        if(RingBuffer_IsFull(fifo) == true)
-        {
-          printf("FIFO满了！！！\n");
-        }
-        else
-        {
-          printf("FIFO没满！！！\n");
-        }
+				if (RingBuffer_IsFull(fifo) == true)
+				{
+					printf("FIFO满了！！！\n");
+				}
+				else
+				{
+					printf("FIFO没满！！！\n");
+				}
 
-        if(RingBuffer_IsEmpty(fifo) == true)
-        {
-          printf("FIFO空了！！！\n");
-        }
-        else
-        {
-          printf("FIFO没空！！！\n");
-        }
-      }
+				if (RingBuffer_IsEmpty(fifo) == true)
+				{
+					printf("FIFO空了！！！\n");
+				}
+				else
+				{
+					printf("FIFO没空！！！\n");
+				}
+			}
 
-      printf("\n");
+			printf("\n");
 
-      {
-        RingBuffer_Reset(fifo);
-        printf("FIFO清空成功，FIFO大小：%d，使用大小：%d，剩余大小：%d\n",
-               RingBuffer_Size(fifo), RingBuffer_Len(fifo), RingBuffer_Avail(fifo));
+			{
+				RingBuffer_Reset(fifo);
+				printf("FIFO清空成功，FIFO大小：%d，使用大小：%d，剩余大小：%d\n",
+					   RingBuffer_Size(fifo), RingBuffer_Len(fifo), RingBuffer_Avail(fifo));
 
-        if(RingBuffer_IsFull(fifo) == true)
-        {
-          printf("FIFO满了！！！\n");
-        }
-        else
-        {
-          printf("FIFO没满！！！\n");
-        }
+				if (RingBuffer_IsFull(fifo) == true)
+				{
+					printf("FIFO满了！！！\n");
+				}
+				else
+				{
+					printf("FIFO没满！！！\n");
+				}
 
-        if(RingBuffer_IsEmpty(fifo) == true)
-        {
-          printf("FIFO空了！！！\n");
-        }
-        else
-        {
-          printf("FIFO没空！！！\n");
-        }
-      }
+				if (RingBuffer_IsEmpty(fifo) == true)
+				{
+					printf("FIFO空了！！！\n");
+				}
+				else
+				{
+					printf("FIFO没空！！！\n");
+				}
+			}
 
-      printf("\n");
+			printf("\n");
 
-      {
-        if(RingBuffer_In(fifo, data, sizeof(data)) > 0)
-        {
-          printf("FIFO写入成功，FIFO大小：%d，使用大小：%d，剩余大小：%d\n",
-                 RingBuffer_Size(fifo), RingBuffer_Len(fifo), RingBuffer_Avail(fifo));
-        }
-        else
-        {
-          printf("FIFO写入失败，FIFO大小：%d，使用大小：%d，剩余大小：%d\n",
-                 RingBuffer_Size(fifo), RingBuffer_Len(fifo), RingBuffer_Avail(fifo));
-        }
+			{
+				if (RingBuffer_In(fifo, data, sizeof(data)) > 0)
+				{
+					printf("FIFO写入成功，FIFO大小：%d，使用大小：%d，剩余大小：%d\n",
+						   RingBuffer_Size(fifo), RingBuffer_Len(fifo), RingBuffer_Avail(fifo));
+				}
+				else
+				{
+					printf("FIFO写入失败，FIFO大小：%d，使用大小：%d，剩余大小：%d\n",
+						   RingBuffer_Size(fifo), RingBuffer_Len(fifo), RingBuffer_Avail(fifo));
+				}
 
-        if(RingBuffer_IsFull(fifo) == true)
-        {
-          printf("FIFO满了！！！\n");
-        }
-        else
-        {
-          printf("FIFO没满！！！\n");
-        }
+				if (RingBuffer_IsFull(fifo) == true)
+				{
+					printf("FIFO满了！！！\n");
+				}
+				else
+				{
+					printf("FIFO没满！！！\n");
+				}
 
-        if(RingBuffer_IsEmpty(fifo) == true)
-        {
-          printf("FIFO空了！！！\n");
-        }
-        else
-        {
-          printf("FIFO没空！！！\n");
-        }
-      }
+				if (RingBuffer_IsEmpty(fifo) == true)
+				{
+					printf("FIFO空了！！！\n");
+				}
+				else
+				{
+					printf("FIFO没空！！！\n");
+				}
+			}
 
-      printf("\n");
+			printf("\n");
 
-      {
-        if(RingBuffer_In(fifo, data, sizeof(data)) > 0)
-        {
-          printf("FIFO写入成功，FIFO大小：%d，使用大小：%d，剩余大小：%d\n",
-                 RingBuffer_Size(fifo), RingBuffer_Len(fifo), RingBuffer_Avail(fifo));
-        }
-        else
-        {
-          printf("FIFO写入失败，FIFO大小：%d，使用大小：%d，剩余大小：%d\n",
-                 RingBuffer_Size(fifo), RingBuffer_Len(fifo), RingBuffer_Avail(fifo));
-        }
+			{
+				if (RingBuffer_In(fifo, data, sizeof(data)) > 0)
+				{
+					printf("FIFO写入成功，FIFO大小：%d，使用大小：%d，剩余大小：%d\n",
+						   RingBuffer_Size(fifo), RingBuffer_Len(fifo), RingBuffer_Avail(fifo));
+				}
+				else
+				{
+					printf("FIFO写入失败，FIFO大小：%d，使用大小：%d，剩余大小：%d\n",
+						   RingBuffer_Size(fifo), RingBuffer_Len(fifo), RingBuffer_Avail(fifo));
+				}
 
-        if(RingBuffer_IsFull(fifo) == true)
-        {
-          printf("FIFO满了！！！\n");
-        }
-        else
-        {
-          printf("FIFO没满！！！\n");
-        }
+				if (RingBuffer_IsFull(fifo) == true)
+				{
+					printf("FIFO满了！！！\n");
+				}
+				else
+				{
+					printf("FIFO没满！！！\n");
+				}
 
-        if(RingBuffer_IsEmpty(fifo) == true)
-        {
-          printf("FIFO空了！！！\n");
-        }
-        else
-        {
-          printf("FIFO没空！！！\n");
-        }
-      }
+				if (RingBuffer_IsEmpty(fifo) == true)
+				{
+					printf("FIFO空了！！！\n");
+				}
+				else
+				{
+					printf("FIFO没空！！！\n");
+				}
+			}
 
-      printf("\n");
+			printf("\n");
 
-      {
-        uint8_t  rdata[256] = {0};
-        uint16_t len        = RingBuffer_Out(fifo, rdata, sizeof(rdata));
+			{
+				uint8_t rdata[256] = {0};
+				uint16_t len = RingBuffer_Out(fifo, rdata, sizeof(rdata));
 
-        if(len > 0)
-        {
-          printf("从FIFO中读出的数据，长度：%d\n", len);
+				if (len > 0)
+				{
+					printf("从FIFO中读出的数据，长度：%d\n", len);
 
-          for(int i = 0; i < len; i++)
-          {
-            printf("%d ", rdata[i]);
-          }
+					for (int i = 0; i < len; i++)
+					{
+						printf("%d ", rdata[i]);
+					}
 
-          printf("\n");
+					printf("\n");
 
-          printf("FIFO读取成功，FIFO大小：%d，使用大小：%d，剩余大小：%d\n",
-                 RingBuffer_Size(fifo), RingBuffer_Len(fifo), RingBuffer_Avail(fifo));
-        }
-        else
-        {
-          printf("FIFO读取失败，FIFO大小：%d，使用大小：%d，剩余大小：%d\n",
-                 RingBuffer_Size(fifo), RingBuffer_Len(fifo), RingBuffer_Avail(fifo));
-        }
+					printf("FIFO读取成功，FIFO大小：%d，使用大小：%d，剩余大小：%d\n",
+						   RingBuffer_Size(fifo), RingBuffer_Len(fifo), RingBuffer_Avail(fifo));
+				}
+				else
+				{
+					printf("FIFO读取失败，FIFO大小：%d，使用大小：%d，剩余大小：%d\n",
+						   RingBuffer_Size(fifo), RingBuffer_Len(fifo), RingBuffer_Avail(fifo));
+				}
 
-        if(RingBuffer_IsFull(fifo) == true)
-        {
-          printf("FIFO满了！！！\n");
-        }
-        else
-        {
-          printf("FIFO没满！！！\n");
-        }
+				if (RingBuffer_IsFull(fifo) == true)
+				{
+					printf("FIFO满了！！！\n");
+				}
+				else
+				{
+					printf("FIFO没满！！！\n");
+				}
 
-        if(RingBuffer_IsEmpty(fifo) == true)
-        {
-          printf("FIFO空了！！！\n");
-        }
-        else
-        {
-          printf("FIFO没空！！！\n");
-        }
-      }
+				if (RingBuffer_IsEmpty(fifo) == true)
+				{
+					printf("FIFO空了！！！\n");
+				}
+				else
+				{
+					printf("FIFO没空！！！\n");
+				}
+			}
 
-      printf("\n");
+			printf("\n");
 
-      {
-        uint8_t  rdata[256] = {0};
-        uint16_t len        = RingBuffer_Out(fifo, rdata, sizeof(rdata));
+			{
+				uint8_t rdata[256] = {0};
+				uint16_t len = RingBuffer_Out(fifo, rdata, sizeof(rdata));
 
-        if(len > 0)
-        {
-          printf("从FIFO中读出的数据，长度：%d\n", len);
+				if (len > 0)
+				{
+					printf("从FIFO中读出的数据，长度：%d\n", len);
 
-          for(int i = 0; i < len; i++)
-          {
-            printf("%d ", rdata[i]);
-          }
+					for (int i = 0; i < len; i++)
+					{
+						printf("%d ", rdata[i]);
+					}
 
-          printf("\n");
+					printf("\n");
 
-          printf("FIFO读取成功，FIFO大小：%d，使用大小：%d，剩余大小：%d\n",
-                 RingBuffer_Size(fifo), RingBuffer_Len(fifo), RingBuffer_Avail(fifo));
-        }
-        else
-        {
-          printf("FIFO读取失败，FIFO大小：%d，使用大小：%d，剩余大小：%d\n",
-                 RingBuffer_Size(fifo), RingBuffer_Len(fifo), RingBuffer_Avail(fifo));
-        }
+					printf("FIFO读取成功，FIFO大小：%d，使用大小：%d，剩余大小：%d\n",
+						   RingBuffer_Size(fifo), RingBuffer_Len(fifo), RingBuffer_Avail(fifo));
+				}
+				else
+				{
+					printf("FIFO读取失败，FIFO大小：%d，使用大小：%d，剩余大小：%d\n",
+						   RingBuffer_Size(fifo), RingBuffer_Len(fifo), RingBuffer_Avail(fifo));
+				}
 
-        if(RingBuffer_IsFull(fifo) == true)
-        {
-          printf("FIFO满了！！！\n");
-        }
-        else
-        {
-          printf("FIFO没满！！！\n");
-        }
+				if (RingBuffer_IsFull(fifo) == true)
+				{
+					printf("FIFO满了！！！\n");
+				}
+				else
+				{
+					printf("FIFO没满！！！\n");
+				}
 
-        if(RingBuffer_IsEmpty(fifo) == true)
-        {
-          printf("FIFO空了！！！\n");
-        }
-        else
-        {
-          printf("FIFO没空！！！\n");
-        }
-      }
+				if (RingBuffer_IsEmpty(fifo) == true)
+				{
+					printf("FIFO空了！！！\n");
+				}
+				else
+				{
+					printf("FIFO没空！！！\n");
+				}
+			}
 
-      printf("\n\n\n");
-      Sleep(5000);
-    }
-  }
-  else
-  {
-    printf("FIFO创建失败\n");
-  }
+			printf("\n\n\n");
+			Sleep(5000);
+		}
+	}
+	else
+	{
+		printf("FIFO创建失败\n");
+	}
 
-  for(;;)
-  {
-
-  }
+	for (;;)
+	{
+	}
 }
